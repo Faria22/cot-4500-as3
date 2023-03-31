1962d2a195f0b8104855e3ae00419a6f0407ee58
Felipe Kalluf Faria <felipefaria@Felipes-MacBook-Air.local>
2023-03-31 12:46:19 -0400

    Changes

--- src/test/prob6.py
+++ src/test/prob6.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def prob6():
-    m = [[9,0,5,2,1],[3,9,1,2,1],[0,1,7,2,3],[4,2,3,12,2],[3,2,4,0,8]]
+    m = [[2,2,1],[2,3,0],[1,0,2]]
     n = len(m)
     val = np.linalg.eigvals(m)
     for v in val:
